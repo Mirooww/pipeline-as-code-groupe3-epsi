9d10e75a84036023e053089e61bb4af9c1040926
Mirooww <jordanlansoypro@gmail.com>
2026-03-04 16:28:57 +0100

    features: Hello World on webapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-  <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-    Hello World
-  </main>
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World </h1>
+    </main>
   );
 }
